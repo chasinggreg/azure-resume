136c20230d1313215ef99db612f565ef8585b75b
chasinggreg <gncsmith@gmail.com>
2025-02-09 19:57:08 -0600

    asfs

--- backend/api/Counter.cs
+++ backend/api/Counter.cs
@@ -5,10 +5,11 @@ namespace Company.Function
 
   public class Counter
   {
-    [JsonProperty(PropertyName"id")]
-    public string ID {get; set;}
-    [JsonProperty(PropertyName = "count")]
+      [JsonProperty(PropertyName="id")]
+      public string Id {get; set;}
+      
+      [JsonProperty(PropertyName="count")]
 
-    public int Count {get; set;}
+      public int Count {get; set;}
   }
 }
